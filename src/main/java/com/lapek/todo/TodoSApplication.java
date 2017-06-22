215de7682745190780dc05d34cd640fe92b0de68
Piotr Łapa <joi1.512@gmail.com>
2017-06-22 22:50:56 +0200

    Initial Commit

---
+++ src/main/java/com/lapek/todo/TodoSApplication.java
@@ -0,0 +1,12 @@
+package com.lapek.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSApplication.class, args);
+	}
+}
